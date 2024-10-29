145a6c56255e60171e366690ab10f0108b91576d
r8vnhill <reachme@ravenhill.cl>
2024-10-28 22:30:48 -0300

    🔄 Refactor module setup task for anonymous objects
    - 🔧 Converted `setupAnonymousModule` task to use `ModuleSetupTask` class for improved structure and reusability.
    - 🛠️ Simplified file creation logic by organizing main and test file mappings more clearly.

--- convention-plugins/src/main/kotlin/anonymous.gradle.kts
+++ convention-plugins/src/main/kotlin/anonymous.gradle.kts
@@ -1,51 +1,24 @@
-val baseDir = "anonymous"
-val baseDirFile = rootProject.file(baseDir)
+import tasks.ModuleSetupTask
 
-tasks.register("setupAnonymousModule") {
+tasks.register<ModuleSetupTask>("setupAnonymousModule") {
+    description = "Creates the base module and files for the anonymous objects lesson"
+    module.set("anonymous")
+
     doLast {
-        // Crear el directorio base del módulo y build.gradle.kts si no existe
-        baseDirFile.mkdirs()
-        createGradleBuild(baseDirFile)
-        val main = "main"
-        val test = "test"
-        val anonymous = "anonymous"
-
-        createFiles(anonymous, test to "AnonymousObjectTest.kt")
         createFiles(
-            "$anonymous/validation",
-            main to "Validator.kt", test to "ValidatorTest.kt"
+            "anonymous",
+            test to "AnonymousObjectTest.kt"
         )
         createFiles(
-            "$anonymous/people",
-            main to "Person.kt", main to "AbstractPerson.kt",
+            "validation",
+            main to "Validator.kt",
+            test to "ValidatorTest.kt"
+        )
+        createFiles(
+            "people",
+            main to "Person.kt",
+            test to "AbstractPerson.kt",
             test to "PersonTest.kt"
         )
-    }
-}
-
-fun createGradleBuild(baseDir: File) = baseDir.resolve("build.gradle.kts").apply {
-    if (!exists()) {
-        writeText("// Intentionally left blank")
-        println("File created: $this")
-    } else {
-        println("File already exists: $this")
-    }
-}
-
-fun createFiles(packageName: String, vararg files: Pair<String, String>) {
-    val group = rootProject.group.toString().replace(".", "/")
-    files.forEach { (type, fileName) ->
-        baseDirFile.resolve(
-            "src/$type/kotlin/${group.replace(".", "/")}/$packageName"
-        )
-            .apply { mkdirs() }
-            .resolve(fileName).apply {
-                if (!exists()) {
-                    writeText("package $group.repo\n\n")
-                    println("File created: $this")
-                } else {
-                    println("File already exists: $this")
-                }
-            }
     }
 }
